b16876d25b9387306138699a26b53bd72a5323cc
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-02 14:07:06 -0600

    Added dietary restriction indications to ItemRow

--- iDIne/ItemRow.swift
+++ iDIne/ItemRow.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct ItemRow: View {
     var item: MenuItem
+    static let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }
+            Spacer()
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(Self.colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
