0fb1b42049f4467e576092a8d74a291604abbc00
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-02 11:29:54 -0600

    Initial Commit

---
+++ iDIne/iDIneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDIneApp.swift
+//  iDIne
+//
+//  Created by Tom Bredemeier on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct iDIneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
