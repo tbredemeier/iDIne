430a639f7b9c344cf0883d9f5d8bfef69eebffb5
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-02 14:26:25 -0600

    Added ItemDetail view

---
+++ iDIne/ItemDetail.swift
@@ -0,0 +1,38 @@
+//
+//  ItemDetail.swift
+//  iDIne
+//
+//  Created by Tom Bredemeier on 2/2/21.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(Color.black)
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .offset(x: -5, y: -5)
+            }
+            Text(item.description)
+                .padding()
+            Spacer()
+        }
+        .navigationBarTitle(Text(item.name), displayMode: .inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ItemDetail(item: MenuItem.example)
+        }
+    }
+}
