5c8d56a9d1b95095d117bf49bd7a61bb9f0c8624
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-03 20:19:05 -0600

    Created OrderView

---
+++ iDIne/OrderView.swift
@@ -0,0 +1,43 @@
+//
+//  OrderView.swift
+//  iDIne
+//
+//  Created by Tom Bredemeier on 2/3/21.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Check Out")) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationBarTitle("Order")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static let order = Order()
+    
+    static var previews: some View {
+        OrderView().environmentObject(order)
+    }
+}
--- iDIne/iDIneApp.swift
+++ iDIne/iDIneApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct iDIneApp: App {
+    var order = Order()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(order)
         }
     }
 }
